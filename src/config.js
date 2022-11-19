8e61b93b1031df7acc548366593220410144aab1
0x1381 <nullx1381@gmail.com>
2022-11-20 01:55:16 +0330

    initial commit

---
+++ src/config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  siteTitle: 'Afshin Ataei | Software Developer | Content Freelancer',
+  siteDescription:
+    'Afshin Ataei is a Software Developer at Amdocs, who loves learning new things and helping tech beginners.',
+  siteKeywords:
+    'Afshin Ataei, software developer, software engineer, content freelancer, face mask, face mask detection, web developer, java developer, pepper content, jamshedpur, amdocs, bit, mesra',
+  siteUrl: 'https://0x1381.github.io/Protfilo-/',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Chandrika Deb',
+  location: 'Isfahan, Iran',
+  email: 'nullx1381@gmail.com',
+  github: 'https://github.com/0x1381',
+  twitterHandle: '@0x1381',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/0x1381',
+    },
+    {
+      name: 'Linkedin',
+      url: '',
+    },
+    {
+      name: 'Codepen',
+      url: '',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/afshin__.tork',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/0x1381',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Projects',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: false,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
