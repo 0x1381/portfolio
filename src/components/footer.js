851b75fabb9a22826f53574d63732861c9e364ad
Afshin <53143786+0x1381@users.noreply.github.com>
2022-12-14 01:00:03 +0330

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,8 +106,7 @@ const Footer = () => {
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by 0x1381
+            Designed &amp; Built by 0x1381
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
